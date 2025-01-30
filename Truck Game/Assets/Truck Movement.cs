8bcbcc88814a1d8a23cadea8a6fbaf4907da72a4
Dman7713 <PeytonPrivratsky@gmail.com>
2025-01-30 10:28:50 -0800

    Added a Truck

---
+++ Truck Game/Assets/Truck Movement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TruckMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(transform.forward * speed, ForceMode.Acceleration);
+    }
+}
